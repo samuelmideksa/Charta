84381d320d06c7994f023399f254db89e2ccc42c
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-03-19 14:22:08 +0300

    end '/' removed

--- charta_library/urls.py
+++ charta_library/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('explore', views.explore, name='explore'),
     path('explore/<int:genre_id>/<slug:genre_name>', views.explore_genre, name='explore_genre'),
     path('author/<int:author_id>/<slug:author_name>', views.author_details, name='author_details'),
-    path('search/', views.search_results, name='search_results'),
+    path('search', views.search_results, name='search_results'),
     path('book/<int:book_id>/<slug:book_title>', views.book_details, name='book_details'),
-    path('series/<int:series_id>/<slug:series_title>/', views.series_details, name='series_details'),
+    path('series/<int:series_id>/<slug:series_title>', views.series_details, name='series_details'),
 ]
